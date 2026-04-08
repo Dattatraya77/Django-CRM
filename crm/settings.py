f4be5a9efc1a87ffd1a28561c288714ba97d6004
Dattatraya Walunj <drwalunj.2010@gmail.com>
2026-04-08 17:53:26 +0530

    added whitenoise

--- crm/settings.py
+++ crm/settings.py
@@ -128,7 +128,11 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
