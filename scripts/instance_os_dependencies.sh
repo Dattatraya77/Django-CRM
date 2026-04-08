7bdf29666d0717596c3f8cde6f827303974660ad
Dattatraya Walunj <drwalunj.2010@gmail.com>
2026-04-08 17:19:56 +0530

    update .sh  and settings.py files

--- scripts/instance_os_dependencies.sh
+++ scripts/instance_os_dependencies.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
-sudo apt update
+sudo apt install -y \
+build-essential \
+python3-dev \
+libpq-dev \
+libjpeg-dev \
+zlib1g-dev \
+libfreetype6-dev \
+liblcms2-dev \
+libwebp-dev \
+libtiff5-dev \
+libopenjp2-7-dev \
+libharfbuzz-dev \
+libfribidi-dev \
+libxcb1-dev
+
 # Install Python3 pip
 sudo apt install -y python3-pip
 
